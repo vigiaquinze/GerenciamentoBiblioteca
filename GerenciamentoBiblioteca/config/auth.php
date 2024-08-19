24ee9025856f78831bb65097a5a07bc3b97813e7
vigiaquinze <victorhugopagliuca84@gmail.com>
2024-08-19 16:55:41 -0300

    19.8 17h

--- GerenciamentoBiblioteca/config/auth.php
+++ GerenciamentoBiblioteca/config/auth.php
@@ -14,8 +14,8 @@ return [
     */
 
     'defaults' => [
-        'guard' => env('AUTH_GUARD', 'web'),
-        'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
+        'guard' => 'usuario',
+        'passwords' => 'users',
     ],
 
     /*
@@ -36,11 +36,17 @@ return [
     */
 
     'guards' => [
-        'web' => [
+        'usuario' => [
             'driver' => 'session',
+            'provider' => 'usuario',
+        ],
+
+        'api' => [
+            'driver' => 'token',
             'provider' => 'users',
         ],
     ],
+
 
     /*
     |--------------------------------------------------------------------------
@@ -60,9 +66,9 @@ return [
     */
 
     'providers' => [
-        'users' => [
+        'usuario' => [
             'driver' => 'eloquent',
-            'model' => env('AUTH_MODEL', App\Models\User::class),
+            'model' => App\Models\Usuario::class,
         ],
 
         // 'users' => [
@@ -93,9 +99,8 @@ return [
     'passwords' => [
         'users' => [
             'provider' => 'users',
-            'table' => env('AUTH_PASSWORD_RESET_TOKEN_TABLE', 'password_reset_tokens'),
+            'table' => 'password_resets',
             'expire' => 60,
-            'throttle' => 60,
         ],
     ],
 
